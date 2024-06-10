1c879acd6f69438d465e923428d85ab3eeae1a42
Ayat Nour Hachmi <97694707+ayatnourhachmi@users.noreply.github.com>
2024-06-10 20:04:24 +0100

    Update sort_files.py VF

--- sort_files.py
+++ sort_files.py
@@ -4,23 +4,22 @@ import shutil
 import calendar
 from datetime import datetime
 
-# convert the numeric month obtained from the file creation date into its corresponding English name.
 def get_month_name(month_number):
+    """ Convert the numeric month obtained from the file creation date into its corresponding English name. """
     try:
         month_name = calendar.month_name[month_number]
         return month_name
     except IndexError:
         return "Invalid month number"
 
-
 def sort_files(source_path, destination_path):
+    """ Sort files from source_path to destination_path based on their creation date and file extension. """
     # Create destination directory if it doesn't exist
     if not os.path.exists(destination_path):
         os.makedirs(destination_path)
 
     # Get a list of files in the source directory
-    files = [f for f in os.listdir(source_path) if os.path.isfile(
-        os.path.join(source_path, f))]
+    files = [f for f in os.listdir(source_path) if os.path.isfile(os.path.join(source_path, f))]
 
     for file in files:
         source_file_path = os.path.join(source_path, file)
@@ -33,12 +32,9 @@ def sort_files(source_path, destination_path):
         # Create destination directories based on extension, year, and month
         extension_path = os.path.join(destination_path, file_extension[1:])
         year_path = os.path.join(extension_path, str(creation_date.year))
-        month_path = os.path.join(year_path, str(
-            get_month_name(creation_date.month)))
+        month_path = os.path.join(year_path, get_month_name(creation_date.month))
 
         # Create directories if they don't exist
-        os.makedirs(extension_path, exist_ok=True)
-        os.makedirs(year_path, exist_ok=True)
         os.makedirs(month_path, exist_ok=True)
 
         # Construct the destination file path
@@ -48,7 +44,6 @@ def sort_files(source_path, destination_path):
         shutil.move(source_file_path, destination_file_path)
 
         print(f"Moved: {source_file_path} -> {destination_file_path}")
-
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
